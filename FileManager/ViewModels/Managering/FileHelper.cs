423b119146584e5479d1ce1adb368b59d2aa871e
Aleksey <aleksey.dantist@gmail.com>
2022-09-14 15:02:50 +0600

    feat: create file helper

---
+++ FileManager/ViewModels/Managering/FileHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileManager.ViewModels.Managering
+{
+    public static class FileHelper
+    {
+
+        public static int GetFilesCount(DirectoryInfo d)
+        {
+            try
+            {
+                return d.GetFiles().Count() + d.GetDirectories().Count();
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return 0;
+            }
+        }
+
+        public static bool CheckFileOrFolder(string path)
+        {
+            try
+            {
+                FileAttributes attr = File.GetAttributes(path);
+                if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+    }
+}
